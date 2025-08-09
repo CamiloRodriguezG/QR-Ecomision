3b743cf6e716086b52fc3055088c82533eb6e588
CamiloRodriguezG <72152781+CamiloRodriguezG@users.noreply.github.com>
2025-08-09 16:47:40 -0500

    Tercer avance

--- frontend/js/formulario.js
+++ frontend/js/formulario.js
@@ -1,16 +1,16 @@
-const formIngreso = document.querySelector(".form-ingreso");
-const formDatosPersonales = document.getElementById("form-datos-personales");
-const nombreInput = document.getElementById("nombre");
-const apellidoInput = document.getElementById("apellido");
-const numeroPregunta = document.getElementById("numero-pregunta");
-const textoBienvenida = document.querySelector(".texto-bienvenida");
-const formContainer = document.getElementById("form-container");
-const preguntas = document.querySelectorAll(".pregunta-contenedor");
-const btnAnterior = document.getElementById("anterior-btn");
-const btnSiguiente = document.getElementById("siguiente-btn");
-const opcionesPreguntas = document.querySelectorAll(".opcion-pregunta");
-const numeroUsuarios = document.getElementById("numero-usuarios");
-const barraAvance = document.querySelector(".barra-avance");
+const formIngreso = document.querySelector('.form-ingreso');
+const formDatosPersonales = document.getElementById('form-datos-personales');
+const nombreInput = document.getElementById('nombre');
+const numeroPregunta = document.getElementById('numero-pregunta');
+const textoBienvenida = document.querySelector('.texto-bienvenida');
+const formContainer = document.getElementById('form-container');
+const preguntas = document.querySelectorAll('.pregunta-contenedor');
+const imagenLucian = document.querySelector('.imagen-lucian');
+const btnAnterior = document.getElementById('anterior-btn');
+const btnSiguiente = document.getElementById('siguiente-btn');
+const opcionesPreguntas = document.querySelectorAll('.opcion-pregunta');
+const numeroUsuarios = document.getElementById('numero-usuarios');
+const barraAvance = document.querySelector('.barra-avance');
 
 let preguntasTotales = preguntas.length;
 let indiceActual = 0;
@@ -19,96 +19,87 @@ let avance = 0;
 const socket = io();
 
 // Evento para el formulario de ingreso
-formDatosPersonales.addEventListener("submit", (e) => {
+formDatosPersonales.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const nombre = nombreInput.value.trim();
-  const apellido = apellidoInput.value.trim();
 
-  if (nombre && apellido) {
-    // Guardar los datos del usuario (puedes expandir esto según necesites)
-    localStorage.setItem("usuarioNombre", nombre);
-    localStorage.setItem("usuarioApellido", apellido);
+  if (nombre) {
+    localStorage.setItem('usuarioNombreQREM', nombre);
 
     // Ocultar el formulario de ingreso
-    formIngreso.classList.add("oculto");
+    formIngreso.classList.add('oculto');
 
     // Iniciar el test
     iniciarTest();
   }
 });
 
-socket.on("userCount", (cantidad) => {
-  numeroUsuarios.style.opacity = "0";
+socket.on('userCount', (cantidad) => {
+  numeroUsuarios.style.opacity = '0';
   setTimeout(() => {
     numeroUsuarios.textContent = cantidad;
-    numeroUsuarios.style.opacity = "1";
+    numeroUsuarios.style.opacity = '1';
   }, 500);
 });
 
 opcionesPreguntas.forEach((op) => {
-  op.addEventListener("click", () => {
+  op.addEventListener('click', () => {
     // Obtener el contenedor de opciones (ul) de la pregunta actual
-    const contenedorOpciones = op.closest(".opciones-contenedor");
+    const contenedorOpciones = op.closest('.opciones-contenedor');
 
-    contenedorOpciones
-      .querySelectorAll(".opcion-pregunta")
-      .forEach((opcion) => {
-        opcion.classList.remove("opcion-seleccionada");
-      });
+    contenedorOpciones.querySelectorAll('.opcion-pregunta').forEach((opcion) => {
+      opcion.classList.remove('opcion-seleccionada');
+    });
 
-    op.classList.add("opcion-seleccionada");
+    op.classList.add('opcion-seleccionada');
   });
 });
 
 function mostrarPregunta(nueva) {
   const preguntaActual = preguntas[indiceActual];
-  preguntaActual.classList.remove("active");
-  preguntaActual.classList.add(nueva > indiceActual ? "exit-up" : "exit-down");
+  preguntaActual.classList.remove('active');
+  preguntaActual.classList.add(nueva > indiceActual ? 'exit-up' : 'exit-down');
 
   indiceActual = nueva;
 
   const siguiente = preguntas[indiceActual];
-  siguiente.classList.remove("exit-up", "exit-down");
-  siguiente.classList.add("active");
+  siguiente.classList.remove('exit-up', 'exit-down');
+  siguiente.classList.add('active');
 
   btnAnterior.disabled = indiceActual === 0;
   if (indiceActual === 0) {
-    btnAnterior.classList.add("btn-desactivado");
+    btnAnterior.classList.add('btn-desactivado');
   } else {
-    btnAnterior.classList.remove("btn-desactivado");
+    btnAnterior.classList.remove('btn-desactivado');
   }
 
   if (indiceActual === preguntas.length - 1) {
-    btnSiguiente.textContent = "Finalizar";
-    btnSiguiente.style.background = "var(--color-azul)";
+    btnSiguiente.textContent = 'Finalizar';
+    btnSiguiente.style.background = 'var(--color-azul)';
   } else {
-    btnSiguiente.textContent = "Siguiente";
-    btnSiguiente.style.background = "";
+    btnSiguiente.textContent = 'Siguiente';
+    btnSiguiente.style.background = '';
   }
 
   avance = (indiceActual * 100) / preguntasTotales;
   barraAvance.style.width = `${avance}%`;
 }
 
-btnSiguiente.addEventListener("click", () => {
+btnSiguiente.addEventListener('click', () => {
   const preguntaActual = preguntas[indiceActual];
 
   // Verificar si hay alguna opción seleccionada en la pregunta actual
-  const opcionSeleccionada = preguntaActual.querySelector(
-    ".opcion-seleccionada"
-  );
+  const opcionSeleccionada = preguntaActual.querySelector('.opcion-seleccionada');
 
   if (!opcionSeleccionada) {
     // Obtener el contenedor de opciones y activar la animación de terremoto
-    const contenedorOpciones = preguntaActual.querySelector(
-      ".opciones-contenedor"
-    );
-    contenedorOpciones.classList.add("terremoto");
+    const contenedorOpciones = preguntaActual.querySelector('.opciones-contenedor');
+    contenedorOpciones.classList.add('terremoto');
 
     // Remover la clase después de que termine la animación
     setTimeout(() => {
-      contenedorOpciones.classList.remove("terremoto");
+      contenedorOpciones.classList.remove('terremoto');
     }, 1000);
 
     return;
@@ -120,22 +111,32 @@ btnSiguiente.addEventListener("click", () => {
   } else {
     let opcionesSeleccionadas = [];
     opcionesPreguntas.forEach((op) => {
-      if (op.classList.contains("opcion-seleccionada")) {
+      if (op.classList.contains('opcion-seleccionada')) {
         opcionesSeleccionadas.push(op.textContent);
       }
     });
-    const nombre = localStorage.getItem("usuarioNombre");
-    const apellido = localStorage.getItem("usuarioApellido");
-    const opciones = "";
-    alert(
-      `Usuario ${nombre} ${apellido} con siguientes ocpiones\n${opcionesSeleccionadas}`
-    );
-    formContainer.style.display = "none";
-    // Logica de mostrar personaje
+    const nombre = localStorage.getItem('usuarioNombre');
+    const apellido = localStorage.getItem('usuarioApellido');
+
+    /*
+    Logica de resultados
+    */
+
+    preguntaActual.classList.add('deshabilitado');
+    numeroPregunta.classList.add('deshabilitado');
+    imagenLucian.classList.add('deshabilitado');
+    btnAnterior.classList.add('deshabilitado');
+    btnSiguiente.classList.add('deshabilitado');
+    barraAvance.style.width = '100%';
+    barraAvance.classList.add('deshabilitado');
+    document.getElementById('resultados-container').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   }
 });
 
-btnAnterior.addEventListener("click", () => {
+btnAnterior.addEventListener('click', () => {
   if (indiceActual > 0) {
     mostrarPregunta(indiceActual - 1);
     numeroPregunta.textContent = `Pregunta No. ${indiceActual + 1}`;
@@ -144,24 +145,23 @@ btnAnterior.addEventListener("click", () => {
 
 // Función para iniciar el test
 function iniciarTest() {
-  const nombre = localStorage.getItem("usuarioNombre");
-  textoBienvenida.textContent = `Bienvenido ${nombre}, descubramos que guardian verde eres`;
+  const nombre = localStorage.getItem('usuarioNombreQREM');
+  textoBienvenida.innerHTML = `<h1>¡Bienvenido <span class="nombre-bienvenida">${nombre}</span>!<br>descubramos que guardian verde eres</h1>`;
   // Mostrar la primera pregunta
-  preguntas[0].classList.add("active");
-  numeroPregunta.textContent = "Pregunta No. 1";
+  preguntas[0].classList.add('active');
+  numeroPregunta.textContent = 'Pregunta No. 1';
 }
 
 // Función para verificar si ya existen datos del usuario
 function verificarDatosUsuario() {
-  const nombre = localStorage.getItem("usuarioNombre");
-  const apellido = localStorage.getItem("usuarioApellido");
+  const nombre = localStorage.getItem('usuarioNombreQREM');
 
-  if (nombre && apellido) {
-    // Si ya existen los datos, ocultar el formulario y iniciar el test
-    formIngreso.classList.add("oculto");
+  if (nombre) {
+    // Si ya existen los datos, ocultar el formulario e iniciar el test
+    formIngreso.classList.add('oculto');
     iniciarTest();
   }
 }
 
 // Verificar datos al cargar la página
-document.addEventListener("DOMContentLoaded", verificarDatosUsuario);
+document.addEventListener('DOMContentLoaded', verificarDatosUsuario);
--- frontend/js/historia.js
+++ frontend/js/historia.js
@@ -1,48 +1,43 @@
-const textoHistoria = document.getElementById("contenedor-texto-historia");
-const ilustracion = document.getElementById("contenedor-ilustracion");
-const selecciones = document.getElementById("contenedor-selecciones");
+const textoHistoria = document.getElementById('contenedor-texto-historia');
+const ilustracion = document.getElementById('contenedor-ilustracion');
+const selecciones = document.getElementById('contenedor-selecciones');
 
 let ubicacionHistoria = 0;
 
 const imagenes = {
   Lucian: {
-    normal: [
-      "img/lucian/Lucian_1.png",
-      "img/lucian/Lucian_3.png",
-      "img/lucian/Lucian_5.png",
-    ],
-    incorrecto: ["img/lucian/Lucian_2.png", "img/lucian/Lucian_4.png"],
+    normal: ['img/lucian/Lucian_1.png', 'img/lucian/Lucian_3.png', 'img/lucian/Lucian_5.png'],
+    incorrecto: ['img/lucian/Lucian_2.png', 'img/lucian/Lucian_4.png'],
   },
 };
 
 const partesHistoria = {
   0: {
-    texto:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.ipsum dolor sit amet consectetur adipisicing elit.",
-    opciones: ["Opción 1", "Opción 2"],
+    texto: [
+      'Bienvenido, amigo. Hoy descubriremos juntos cómo cada uno de nuestros actos impacta este lugar.',
+      'Ven, acompáñame junto a dos guardianes muy especiales...',
+    ],
+    opciones: [],
     opcionCorrecta: -1,
-    personaje: "Lucian",
+    personajes: ['Lucian'],
   },
   1: {
-    texto:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    opciones: ["Opción 1", "Opción 2"],
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    opciones: ['Opción 1', 'Opción 2'],
     opcionCorrecta: -1,
-    personaje: "Lucian",
+    personaje: 'Lucian',
   },
   2: {
-    texto:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    opciones: ["Opción 1", "Opción 2"],
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    opciones: ['Opción 1', 'Opción 2'],
     opcionCorrecta: -1,
-    personaje: "Lucian",
+    personaje: 'Lucian',
   },
   3: {
-    texto:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    opciones: ["Opción 1", "Opción 2"],
+    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    opciones: ['Opción 1', 'Opción 2'],
     opcionCorrecta: -1,
-    personaje: "Lucian",
+    personaje: 'Lucian',
   },
 };
 
@@ -57,8 +52,7 @@ function iniciarHistoria() {
   const personaje = parteHistoria.personaje;
   // Obtener una imagen aleatoria del array normal
   const imagenesNormales = imagenes[personaje].normal;
-  const imagenAleatoria =
-    imagenesNormales[Math.floor(Math.random() * imagenesNormales.length)];
+  const imagenAleatoria = imagenesNormales[Math.floor(Math.random() * imagenesNormales.length)];
 
   ilustracion.innerHTML = `
     <img src="${imagenAleatoria}" alt="Ilustracion">
@@ -74,4 +68,4 @@ function elegirOpcion(opcionSeleccionada) {
   // logica de avance en la historia
 }
 
-document.addEventListener("DOMContentLoaded", iniciarHistoria);
+document.addEventListener('DOMContentLoaded', iniciarHistoria);
