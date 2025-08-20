9bf8740adc176c30220a51a9dbf0437a3a7b0603
camilo.rod.gut <camilo.rod.gut@gmail.com>
2025-08-20 00:09:51 +0000

    Correccion paginacion y puntaje en hsitoria

--- frontend/js/historia.js
+++ frontend/js/historia.js
@@ -1,3 +1,54 @@
+let aciertos = 0, fallos = 0;
+let seleccionesCorrecta = {
+  3: {
+    correcta: 'Contenedor verde',
+    siguiente: 'p4',
+    imagenLucianCorrecta: './img/lucian/Lucian_N5.png',
+    imagenLucianIncorrecta: './img/lucian/Lucian_N4.png',
+    imagenNairaCorrecta: './img/naira/Naira_2.png',
+    imagenNairaIncorrecta: './img/naira/Naira_3.png',
+  },
+  4: {correcta: 'Contenedor blanco'},
+  5: {correcta: 'Contenedor rojo'}
+}
+
+function pasarPagina(pagina, seleccion){
+  const flipbook = $('#flipbook');
+  if(pagina>=3){
+    let datosPagina = seleccionesCorrecta[pagina];
+    let siguientePaginaClass = datosPagina.siguiente;
+    if(seleccion == datosPagina.correcta){
+      aciertos++;
+      $(`div.${siguientePaginaClass} .contenedor-ilustracion .lucian-vt4`).attr('src', datosPagina.imagenLucianCorrecta);
+      $(`div.${siguientePaginaClass} .contenedor-ilustracion .naira-vt4`).attr('src', datosPagina.imagenNairaCorrecta);
+    }else{
+      fallos++;
+      $(`div.${siguientePaginaClass} .contenedor-ilustracion .lucian-vt4`).attr('src', datosPagina.imagenLucianIncorrecta);
+      $(`div.${siguientePaginaClass} .contenedor-ilustracion .naira-vt4`).attr('src', datosPagina.imagenNairaIncorrecta);
+    }
+  }
+  setTimeout(()=>{
+    if(pagina!=5){
+      flipbook.turn('next');
+    }else{
+      if(fallos>=2){
+        flipbook.turn('page', 7);
+      }else{
+        flipbook.turn('page', 6);
+      }
+    }
+  }, 500);
+}
+
+function reiniciarHistoria(){
+  const flipbook = $('#flipbook');
+  console.log(aciertos);
+  console.log(fallos);
+  aciertos = 0;
+  fallos = 0;
+  flipbook.turn('page', 1);
+}
+
 $(document).ready(function () {
   $('#flipbook').turn({
     width: $('#flipbook').width(),
@@ -6,20 +57,17 @@ $(document).ready(function () {
     display: 'single', // Esto muestra solo una página a la vez,
     duration: 2000,
   });
-
-  // // Pasar página al hacer click en un botón dentro de la página
-  // $('.pagina').on('click', '.contenedor-selecciones button', function () {
-  //   $('#flipbook').turn('next');
-  // });
-
-  // Pasar página al hacer click en un botón dentro de la página
-  $('.contenedor-selecciones button').on('click', function () {
+  
+  // Usar event delegation en el contenedor principal del flipbook
+  $('#flipbook').on('click', '.historia-button', function () {
     const flipbook = $('#flipbook');
-    const totalPages = flipbook.turn('pages');
     const currentPage = flipbook.turn('page');
-    console.log('Página actual:', currentPage, 'Total:', totalPages);
+    const totalPages = flipbook.turn('pages') - 1;
+    const seleccion = $(this).text().trim();
     if (currentPage < totalPages) {
-      flipbook.turn('next');
+      pasarPagina(currentPage, seleccion);
+    }else{
+      reiniciarHistoria();
     }
   });
 });
